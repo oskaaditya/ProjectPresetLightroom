a858d024dfd9a5c813a3c1e8aec929b1ff939666
Zaky98 <zakyluthfansa@gmail.com>
2019-12-16 16:15:05 +0700

    Change apps icon and rename app

--- app/src/main/java/com/example/projectpresetlightroom/activities/MainActivity.java
+++ app/src/main/java/com/example/projectpresetlightroom/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.projectpresetlightroom;
+package com.example.projectpresetlightroom.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.projectpresetlightroom.R;
 
 public class MainActivity extends AppCompatActivity {
 
